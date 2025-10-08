8c57f2ced77262a7a1075b6e7777483f57f806da
gcampolina <gcampolinap@gmail.com>
2025-10-07 22:59:32 -0300

    att back

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
 // rotas de pagamento
 app.use("/api/payments", paymentRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
