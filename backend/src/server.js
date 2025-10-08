b5a8b9f679d6d7d16cd32712d65662f6239e564f
gcampolina <gcampolinap@gmail.com>
2025-10-07 23:58:09 -0300

    teste

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,5 +20,4 @@ app.get("/", (req, res) => {
 // rotas de pagamento
 app.use("/api", paymentRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+export default app;
