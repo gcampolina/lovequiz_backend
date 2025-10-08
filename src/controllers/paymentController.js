a0c736e9a9755bf7c9ccae10caf8beb979cea8eb
gcampolina <gcampolinap@gmail.com>
2025-10-08 17:32:08 -0300

    x

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -12,32 +12,29 @@ export const createPayment = async (req, res) => {
     const preference = new Preference(client);
 
     const response = await preference.create({
-      body: {
-        items: [
-          {
-            title: "LoveQuiz - Resultado do Quiz",
-            quantity: 1,
-            unit_price: parseFloat(amount),
-            currency_id: "BRL",
-          },
-        ],
-        back_urls: {
-          success: "https://yourlovelanguage.netlify.app/result.html?paid=true",
-          failure: "https://yourlovelanguage.netlify.app/result.html?paid=false",
-          pending: "https://yourlovelanguage.netlify.app/result.html?paid=pending",
-        },
-        auto_return: "approved",
-        payment_methods: {
-          excluded_payment_types: [
-            { id: "ticket" }, 
-            { id: "atm" }     
+        body: {
+          items: [
+            {
+              title: "LoveQuiz - Resultado do Quiz",
+              quantity: 1,
+              unit_price: parseFloat(amount),
+              currency_id: "BRL",
+            },
           ],
-          excluded_payment_methods: [],
-          default_payment_method_id: "pix",
-          installments: 1,
+          back_urls: {
+            success: "https://yourlovelanguage.netlify.app/result.html?paid=true",
+            failure: "https://yourlovelanguage.netlify.app/result.html?paid=false",
+            pending: "https://yourlovelanguage.netlify.app/result.html?paid=pending",
+          },
+          auto_return: "approved",
+          payment_methods: {
+            excluded_payment_types: [], // não excluir boleto ou pix
+            excluded_payment_methods: [],
+            installments: 1,
+          },
         },
-      },
     });
+
 
     res.json({
       checkout_url: `https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=${response.id}`,
