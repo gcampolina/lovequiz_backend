466e7c9bdeb5ebf6a4a45d49ae7bb1483bb03a74
gcampolina <gcampolinap@gmail.com>
2025-10-08 17:21:06 -0300

    final

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import paymentRoutes from "../routes/paymentRoutes.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", paymentRoutes);
+
+export default app;
